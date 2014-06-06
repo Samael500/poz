93c0d1aedf6ce6e7fb2493859cfe17419a132edb
Samael500 <Samael500@gmail.com>
2014-06-06 19:09:26 +0400

    feature: restage use .gitignore

---
+++ assets/templates/departament_poz/scripts/slides.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('#slides').slides({
+        preload: true,
+        preloadImage: '/assets/templates/departament_poz/images/slider/loading.gif',
+        play: 5000,
+        pause: 2500,
+        hoverPause: true
+    });
+});
