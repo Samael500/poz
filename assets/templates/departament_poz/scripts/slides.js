fc8c7a7ef074eca7ad74b6e7d557e6f1a6cd32fd
Samael500 <Samael500@gmail.com>
2014-06-06 19:05:32 +0400

    feature: restage files use .gitignore

--- assets/templates/departament_poz/scripts/slides.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $('#slides').slides({
-        preload: true,
-        preloadImage: '/assets/templates/departament_poz/images/slider/loading.gif',
-        play: 5000,
-        pause: 2500,
-        hoverPause: true
-    });
-});
--- assets/templates/departament_poz/scripts/spoiler.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('.spoiler').hide();
-    $('.knob').click(function(){
-        $(this).next().slideToggle("fast");
-        $(this).toggleClass("active");
-    });
-});
