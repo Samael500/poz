1586244e783ea06cae3ce2bb5385681308c8fb82
Samael500 <Samael500@gmail.com>
2014-06-06 19:08:20 +0400

    clear all

--- assets/templates/departament_poz/scripts/maphilight.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	$('.map').maphilight();
-	// $('#squidheadlink').mouseover(function(e) {
-		// $('#squidhead').mouseover();
-	// }).mouseout(function(e) {
-		// $('#squidhead').mouseout();
-	// }).click(function(e) { e.preventDefault(); });
-});
